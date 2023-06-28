08e2c0684f64aeb09844280414261ddce2802b70
Mahika <mahika.radhakrishnan@gmail.com>
2023-06-28 09:51:36 -0600

    Initial Commit

---
+++ evermore/evermoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  evermoreApp.swift
+//  evermore
+//
+//  Created by Scholar on 6/28/23.
+//
+
+import SwiftUI
+
+@main
+struct evermoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
