a8301d1d3af142b21055663d3776001f0d83638f
Mullai Subramaniyan <mullaigs10@gmail.com>
2023-06-28 15:35:49 -0600

    Added the work I've been doing poop

--- evermore/welcome.swift
+++ evermore/welcome.swift
@@ -10,18 +10,65 @@ import SwiftUI
 struct welcome: View {
     var body: some View {
         NavigationStack {
-            VStack {
-                NavigationLink(destination: agenda()) {
-                    Text("agenda")
-                    
-                }
-                NavigationLink(destination: journal()) {
-                    Text("journaling")
-                }
+            ZStack {
                 
-                NavigationLink(destination: pomodoro()) {
-                    Text("pomodoro")
+                Color(red: 126/255, green: 154/255, blue: 132/255_)
+                    .ignoresSafeArea()
+       
+                Image("pine")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(15)
+                
+                VStack {
+                    Text("Welcome:")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.white)
+                    Spacer() .frame(height:50)
+                        Text("Priority Tasks:")
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.white)
+                            .multilineTextAlignment(.leading)
+                    ZStack {
+                        Spacer()
+                            .frame(width:350).frame(height:75)
+                            .background(Rectangle()
+                                .foregroundColor(Color(red: 160/225, green: 182/225, blue: 160/225)))
+                                        .cornerRadius(15)
+                                        .shadow(radius: 15)
+                        NavigationLink(destination: journal()) {
+                            Text("j o u r n a l i n g")
+                        }.font(.title).fontWeight(.bold)
+                            .foregroundColor(Color.white)
+                        
+                    }
+                    ZStack {
+                        
+                        Spacer()
+                            .frame(width:350).frame(height:75)
+                            .background(Rectangle()
+                                .foregroundColor(Color(red: 160/225, green: 182/225, blue: 160/225)))
+                                        .cornerRadius(15)
+                                        .shadow(radius: 15)
+                        NavigationLink(destination: agenda()) {
+                            Text("a g e n d a")
+                                .font(.title)
+                                .fontWeight(.bold)
+                                .foregroundColor(Color.white)
+                            
+                        }
                     
+                        
+                    }
+                   
+                    Spacer();
+                    Text("e v e r m o r e.")
+                    .font(.                                                     largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.white)
+                        .multilineTextAlignment(.center)
                 }
             }
         }
