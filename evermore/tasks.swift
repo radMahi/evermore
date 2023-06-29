aef02913d067a4106140e9fdc06172744003ad45
Mullai Subramaniyan <mullaigs10@gmail.com>
2023-06-29 09:58:16 -0600

    mullai deleted the pomodoro and is editing the tasks

--- evermore/tasks.swift
+++ evermore/tasks.swift
@@ -10,22 +10,45 @@ import SwiftUI
 struct tasks: View {
     var body: some View {
         NavigationStack {
-            VStack {
-                NavigationLink(destination: scheduling()) {
-                    Text("schedule tasks")
-                }
+            ZStack {
+                
+                Color(red: 126/255, green: 154/255, blue: 132/255_)
+                    .ignoresSafeArea()
+                
+                Image("pine")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(15)
+                VStack {
+                    Text("t a s k s:")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.white)
+                    Spacer()
+                    
+                    NavigationLink(destination: scheduling()) {
+                        Text("schedule tasks")
+                    }
                     NavigationLink(destination: welcome()) {
                         Text("back to homepage")
-                }
-                NavigationLink(destination: agenda()) {
-                    Text("back to agenda")
+                    }
+                    NavigationLink(destination: agenda()) {
+                        Text("back to agenda")
+                        
+                    }
+                    Spacer();
+                    Text("e v e r m o r e.")
+                    .font(. largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.white)
+                        .multilineTextAlignment(.center)
                 }
             }
         }
     }
-}
-struct tasks_Previews: PreviewProvider {
-    static var previews: some View {
-        tasks()
+    struct tasks_Previews: PreviewProvider {
+        static var previews: some View {
+            tasks()
+        }
     }
 }
